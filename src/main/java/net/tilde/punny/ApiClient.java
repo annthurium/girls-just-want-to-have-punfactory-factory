1d4c8928ebdfdfe79728c45aba15f15bd5067d20
tilde <tilde@pinterest.com>
2016-12-17 15:26:23 -0800

    makin' some real api calls.

--- src/main/java/net/tilde/punny/ApiClient.java
+++ src/main/java/net/tilde/punny/ApiClient.java
@@ -1,16 +1,32 @@
 package net.tilde.punny;
 
+import java.io.IOException;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
 /**
  * Created by tilde on 12/17/16.
  */
 public class ApiClient {
   private static String URL = "http://rhymebrain.com/talk?function=getRhymes&word=";
   private static String RHYME = "heart";
+  private static OkHttpClient client;
 
   public ApiClient() {
+    this.client = new OkHttpClient();
+
   }
 
-  public String getRhymes(String rhyme) {
-    return "{}";
+  public String getRhymes(String rhyme) throws IOException {
+    Request request = new Request.Builder()
+        .url(this.URL + this.RHYME)
+        .build();
+
+    Response response = client.newCall(request).execute();
+    return response.body().string();
   }
 }
--- src/test/java/net/tilde/punny/ApiClientTest.java
+++ src/test/java/net/tilde/punny/ApiClientTest.java
@@ -2,18 +2,20 @@ package net.tilde.punny;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 /**
  * Created by tilde on 12/17/16.
  */
 public class ApiClientTest {
 
   @Test
- public void testGetRhymes() {
+ public void testGetRhymes() throws IOException {
     ApiClient client = new ApiClient();
     String result = client.getRhymes("foo");
 
     if (result.length() < 1)
       throw new java.lang.RuntimeException();
-
+    System.out.println(result);
   }
 }
