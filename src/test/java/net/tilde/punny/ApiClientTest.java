7e1e14d7f70b8a7e8f198b6ff0e88dbf087d4601
tilde <tilde@pinterest.com>
2016-12-17 15:06:19 -0800

    assert length of getRhyme

--- src/test/java/net/tilde/punny/ApiClientTest.java
+++ src/test/java/net/tilde/punny/ApiClientTest.java
@@ -1,8 +1,19 @@
 package net.tilde.punny;
 
+import org.junit.Test;
+
 /**
  * Created by tilde on 12/17/16.
  */
 public class ApiClientTest {
 
+  @Test
+ public void testGetRhymes() {
+    ApiClient client = new ApiClient();
+    String result = client.getRhymes("foo");
+
+    if (result.length() < 1)
+      throw new java.lang.RuntimeException();
+
+  }
 }
