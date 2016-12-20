a0561fdef2c4a83bbbbe22361b74c1ee815d5887
tilde <tilde@pinterest.com>
2016-12-20 10:51:47 -0800

    loop through JSONArray

--- src/main/java/net/tilde/punny/PunfactoryMain.java
+++ src/main/java/net/tilde/punny/PunfactoryMain.java
@@ -1,17 +1,25 @@
 package net.tilde.punny;
 
 import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
 
 /**
  * Created by tilde on 12/19/16.
  */
 public class PunfactoryMain {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, JSONException {
     ApiClient client = new ApiClient();
     String result = client.getRhymes("foo");
-    System.out.println(result);
 
-    
+    JSONArray array = new JSONArray(result);
+
+    for (int i = 0; i < array.length(); i++) {
+      JSONObject object = array.getJSONObject(i);
+      System.out.println(object);
+      System.out.println("\n");
+    }
 
   }
 }
